093020f48b6abb1e9988b3e26472c82aba005d31
patelmanasi <manasipatel0852@gmail.com>
2026-04-08 11:36:09 +0530

    feat(users): add search, delete functionality, pagination, and improved UI with alerts

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\FormController;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('users/create', [FormController::class, 'create']);
 Route::post('users/create', [FormController::class, 'store'])->name('users.store');
 Route::get('users/list', [FormController::class, 'list']);
